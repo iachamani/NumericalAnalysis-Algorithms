7bbe20b0621f4f7046dc1023ed849af583dc3181
ali <ali@localhost.localdomain>
2022-12-01 13:40:06 +0000

    fixing newton method to calculate square root

--- squareroot.c
+++ squareroot.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+double absol(double n){
+	if(n<0) return -n;
+	return n;
+}
 double squareroot(double num,double precision){
 	double root,y = num;
 	while(1){
-		root = 1/2 * (y+num/y)
-		if(root - y < precision && root - y > -precision){
+		root = 0.5*(y+num/y) ;
+		if(absol(root - y) < precision){
 			break;
 		}
-		y = root
+		y = root ;
 	}
 	return root;
 }
 int main(int argc, char **argv)
 {
-	double x,l=0.00000000001,result;
+	double x,l=0.0000000000001,result;
 	printf("Donner un nombre : ");
-	scanf(&x)
-	result = squareroot(x,l)
-	printf("The result is =  %lf \n",result)
+	scanf("%lf",&x);
+	result = squareroot(x,l);
+	printf("The result is =  %lf \n",result);
 }
